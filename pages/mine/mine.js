2203f3a37f3942420ca0c30e88673c734ded009e
kc3022 <zhengqi.zhang@qq.com>
2020-02-20 17:33:38 +0800

    add file

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,23 +1,46 @@
 // pages/mine/mine.js
+var app = getApp();
 Page({
-
+  
   /**
    * 页面的初始数据
    */
   data: {
-    msg: "hello,mina",
-        indicatorDots:false,//是否显示圆点
-        autoplay:true, //是否自动播放
-        interval: 10000, //自动切换时间间隔
-        duration:2000,//滑动动画时长
-        imgUrls: ['/images/img01.png'] //轮播的图片
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
@@ -67,5 +90,19 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  getUserInfo: function (e) {
+    console.log(e);
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  user_info_handler: function(e){
+    wx.navigateTo({
+      url: 'userinfo',
+    })
   }
 })
