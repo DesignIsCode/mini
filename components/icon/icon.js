6fe51dbb3ed95f513950da3b52cb0c78015886e5
DesignIsCode <344170420@qq.com>
2020-02-26 23:04:29 +0800

    add icon

---
+++ components/icon/icon.js
@@ -0,0 +1,32 @@
+// components/icon/icon.js
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  externalClasses: ['custom-class'],
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    info: null,
+    name: String,
+    size: String,
+    color: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onClick() {
+      this.triggerEvent('click');
+    }
+  }
+})
